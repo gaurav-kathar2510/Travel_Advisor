c0731e46aac574384c7d4603c3abb84473ebd31a
Gaurav Kathar <gkathar221@gmail.com>
2023-07-26 13:22:52 +0530

    scrolled places

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ import Map from './components/Map/Map';
 const App = () => {
 
     const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
 
     const [coords, setCoords] = useState({});
     const [bounds, setBounds] = useState();
+
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -22,10 +25,12 @@ const App = () => {
     useEffect(() => {
         if (bounds) {
             console.log(bounds);
+            setIsLoading(true);
             getPlacesData(bounds.sw, bounds.ne)
                 .then((data) => {
                     console.log(data);
                     setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+                    setIsLoading(false);
                 });
         }
     }, [bounds]);
@@ -36,13 +41,19 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List places={places} />
+                    <List
+                        places={places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                    />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map
                         setCoords={setCoords}
                         setBounds={setBounds}
                         coords={coords}
+                        places={places}
+                        setChildClicked={setChildClicked}
                     />
                 </Grid>
             </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-
-import { LocationOnOutlinedIcon } from '@material-ui/icons/LocationOnOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 // import Rating from '@material-ui/lab'
 
 import useStyles from './styles'
+import { Rating } from '@material-ui/lab';
 
-const Map = ({ coords, setCoords, setBounds }) => {
+const Map = ({ coords, setCoords, setBounds, places, setChildClicked }) => {
     console.log(" this is coord", coords);
     console.log("this is setcoord", setCoords);
     console.log("this is setBound", setBounds);
 
     const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px)');
-
+    const isDesktop = useMediaQuery('(min-width:600px)');
     // const coords = { lat: 0, lng: 0 }
     return (
         <div className={classes.mapContainer}>
@@ -30,8 +29,34 @@ const Map = ({ coords, setCoords, setBounds }) => {
                     setCoords({ lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
                 }}
-                onChildClick={''}
+                onChildClick={(child) => setChildClicked(child)}
             >
+                {places?.map((place, i) => (
+                    <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                    >
+                        {
+                            !isDesktop ? (
+                                <LocationOnOutlinedIcon color='primary' fontSize='large' />
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant='subtitle2' gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                                        alt={place.name}
+                                    />
+                                    <Rating size='small' value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
 
             </GoogleMapReact>
 
